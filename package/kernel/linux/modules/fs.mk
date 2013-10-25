713cc68a47dc0a636b8a0b82fd9d82d076ca5efd
Mo Yi <moyi@moyi-Rev-1-0>
2013-10-26 01:11:35 +0800

    Revert back of Kconfig and mark f2fs as BROKEN
    Signed-off-by: Mo Yi <moyi@moyi-Rev-1-0>

--- package/kernel/linux/modules/fs.mk
+++ package/kernel/linux/modules/fs.mk
@@ -124,6 +124,7 @@ $(eval $(call KernelPackage,fs-ext4))
 define KernelPackage/fs-f2fs
   SUBMENU:=$(FS_MENU)
   TITLE:=F2FS filesystem support (EXPERIMENTAL)
+  DEPENDS:=BROKEN
   KCONFIG:=\
 	CONFIG_F2FS_FS \
 	CONFIG_F2FS_FS_POSIX_ACL=y \
